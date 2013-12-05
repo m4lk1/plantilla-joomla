83171ae01b640f9e1c47bec3aea63e55f9bd011d
m4lk1 <laughingoutloud90@gmail.com>
2013-12-05 12:59:48 +0100

    primera prueba templare dani (error linea 5 php solucionado )

--- templates/prueba/index.php
+++ templates/prueba/index.php
@@ -2,7 +2,7 @@
 	defined( '_JEXEC') or die( 'Restricted access' );
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<? php echo $this->language; ?>"
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<? php echo $this->language; ?>">
 
 <head>
 	<link rel="stylesheet" type="text/css" href="/templates/prueba/style.css" />
